ab2259c2c814ea9511d87b201444e8e3b62cbe35
Jonas <jonysbina@gmail.com>
2021-04-24 12:00:03 +0200

    Test

--- pages/index.js
+++ pages/index.js
@@ -14,43 +14,36 @@ export default function Home() {
           Welcome to IdeaPort's Web
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
+        
         <div className={styles.grid}>
-          <a href="https://ideaport.vercel.app" className={styles.card}>
+          <a href="https://ideaport.vercel.app/aboutapp" className={styles.card}>
             <h3>Documentation &rarr;</h3>
             <p>Find informations about our app.</p>
           </a>
 
-          <a href="https://ideaport.vercel.app" className={styles.card}>
-            <h3>App &rarr;</h3>
+          <a href="https://ideaport.vercel.app/platforms" className={styles.card}>
+            <h3>App</h3>
             <p>Find on what platforms is our app</p>
           </a>
 
           <a
-            href="https://ideaport.vercel.app"
+            href="https://ideaport.vercel.app/aboutcreator"
             className={styles.card}
           >
             <h3>About Creator &rarr;</h3>
             <p>Read somethink about creator of the app</p>
           </a>
  
+          <a
+            href="https://ideaport.vercel.app/aboutcreator"
+            className={styles.card}
+          >
+            <h3>Download &rarr;</h3>
+            <p>Download Idea Port</p>
+          </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    
     </div>
   )
 }
