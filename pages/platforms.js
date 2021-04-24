a423781afe53827ef2851286692a0d532dc74521
Jonas <jonysbina@gmail.com>
2021-04-24 15:01:27 +0200

    Test

--- pages/platforms.js
+++ pages/platforms.js
@@ -1,9 +1,19 @@
 import Head from 'next/head'
+import Image from 'next/image'
 export default function Home() {
     return(
-        <Head>
+        <div className={styles.container}>
+           <Head>
         <title>Idea Port platforms</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Image
+        src="/android.png"
+        alt="Android Logo"
+        width={500}
+        height={500}  
+      />
+        </div>
+        
     )
 }
